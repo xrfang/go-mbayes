be01d689a7c93146c341e614200be5d00bfb785d
xrfang <xrfang@gmail.com>
2016-09-29 23:44:51 +0800

    updated example, adding trainer

---
+++ examples/train/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/xrfang/go-mbayes"
+)
+
+func assert(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+var (
+	r *regexp.Regexp
+	c *mbayes.Classifier
+)
+
+func init() {
+	r = regexp.MustCompile("[a-zA-Z0-9]+")
+}
+
+func train(path string) {
+	cat := filepath.Base(filepath.Dir(path))
+	f, err := os.Open(path)
+	assert(err)
+	defer f.Close()
+	data, err := ioutil.ReadAll(f)
+	fmt.Println(cat, path)
+	tokens := r.FindAll(data, len(data))
+	c.Train(cat, tokens...)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("USAGE: %s <dbname> <training-data-dir>\n", filepath.Base(
+			os.Args[0]))
+		return
+	}
+	db := os.Args[1]
+	root := os.Args[2]
+	var err error
+	c, err = mbayes.Open(db)
+	assert(err)
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		train(path)
+		return nil
+	})
+}
